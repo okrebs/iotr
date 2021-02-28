f9a47ab0066f1275709d0a841705263cd18a7739
okrebs <oliver.b.krebs@gmail.com>
2021-02-28 18:20:04 +0100

    add help files created by roxygen

---
+++ man/examples/wiod.R
@@ -0,0 +1,16 @@
+library(iotr)
+library(dplyr)
+
+wiot <- get_wiot(cache_dir = tempdir(), years = 2014)
+wiot_long <- wiot2long(wiot)
+
+# get wiot into standard long IO-table format
+iot <- wiot_long %>%
+  filter(Country != "TOT") %>%
+  select(origin = Country, sector = RNr, destination, use, flow)
+
+iot <- remove_negative_vad(iot, category_to_scale = 57)
+
+iot <- remove_dynamic_categories(iot,
+                                 dynamic_categories = c(60, 61),
+                                 category_to_scale = 57)
