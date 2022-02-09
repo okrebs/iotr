f9a486d4e124b8b28f59c692f23051819b470a43
okrebs <oliver.b.krebs@gmail.com>
2022-02-09 18:37:17 +0100

    add new prefix to example

--- man/examples/wiod.R
+++ man/examples/wiod.R
@@ -1,16 +1,16 @@
 library(iotr)
 library(dplyr)
 
-wiot <- get_wiot(cache_dir = tempdir(), years = 2014)
-wiot_long <- wiot2long(wiot)
+wiot <- io_get_wiot(cache_dir = tempdir(), years = 2014)
+wiot_long <- io_wiot2long(wiot)
 
 # get wiot into standard long IO-table format
 iot <- wiot_long %>%
   filter(Country != "TOT") %>%
   select(origin = Country, sector = RNr, destination, use, flow)
 
-iot <- rm_negative_vad(iot, category_to_scale = 57)
+iot <- io_rm_negative_vad(iot, category_to_scale = 57)
 
-iot <- rm_dynamics(iot, dynamic_categories = c(60, 61), category_to_scale = 57)
+iot <- io_rm_dynamics(iot, dynamic_categories = c(60, 61), category_to_scale = 57)
 
-iot <- gen_own_trade(iot, max_replace = 1e-6)
+iot <- io_gen_own_trade(iot, max_replace = 1e-6)
